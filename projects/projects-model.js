c5db57844a9ef54cfc680e9cdef13c80b6fd0b37
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-02-21 09:44:07 -0800

    ran seed files and wrote functions in projects-mode.js

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -14,39 +14,55 @@ module.exports = {
     getProjectByID
 }
 
-
+// retrieve a list of all resources
 function getResources() {
-
+    return database("resources");
 }
 
+// retrieve a list of all projects
 function getProjects() {
-    
+    return database("projects");
 }
 
+// retrieve a list of all tasks
 function getTasks() {
-    
+    return database("tasks");
 }
 
+// retrieve a list of all contexts
 function getContexts() {
-    
+    return database("contexts");
 }
 
-function addResource() {
-    
+// add a resource to the database
+function addResource(resource) {
+    return database("resource")
+        .insert(resource)
+        // .then(ids => { return getResourceByID(ids[0]) })
 }
 
-function addProject() {
-    
+// add a project to the database
+function addProject(project) {
+    return database("project")
+        .insert(project)
+        // .then(ids => { return getProjectByID(ids[0]) })
 }
 
-function addTask() {
-    
+// add a task to the database
+function addTask(task) {
+    return database("task")
+        .insert(task)
+        // .then(ids => { return getTaskByID(ids[0]) })
 }
 
-function addContext() {
-    
+// add a context to the database
+function addContext(context) {
+    return database("context")
+        .insert(context)
+        // .then(ids => { return getContextByID(ids[0]) })
 }
 
+// get a project by ID
 function getProjectByID() {
     
 }
